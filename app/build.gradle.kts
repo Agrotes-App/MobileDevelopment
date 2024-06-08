b56b0b2a2f589bff36d5403f9aa9bcbcaad49e2b
ridzky rahim <ridzkyrhm37@gmail.com>
2024-06-09 06:14:21 +0700

    add feature: bottom bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,11 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
 
     //smooth bottom bar
-    implementation (libs.smoothbottombar)
+    implementation(libs.chip.navigation.bar)
+
+    //navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/agrotes_mobile/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/agrotes_mobile/ui/activities/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.agrotes_mobile.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.PopupMenu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.agrotes_mobile.R
 import com.example.agrotes_mobile.databinding.ActivityMainBinding
+import com.example.agrotes_mobile.ui.fragment.HistoryFragment
+import com.example.agrotes_mobile.ui.fragment.MainFragment
+import com.example.agrotes_mobile.ui.fragment.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -24,6 +35,44 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        initFragment()
+        setupBottomBar()
+        setupButton()
+
+    }
+
+    private fun initFragment() {
+        binding.bottomBar.setItemSelected(R.id.navigation_main)
+
+        val mainFragment = MainFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment, mainFragment)
+            .commit()
+    }
+
+    private fun setupBottomBar() {
+        binding.bottomBar.setOnItemSelectedListener {menu ->
+            when(menu){
+                R.id.navigation_main -> {
+                   initFragment()
+                }
+                R.id.navigation_history -> {
+                    val historyFragment = HistoryFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment, historyFragment)
+                        .commit()
+                }
+                R.id.navigation_profile -> {
+                    val profileFragment = ProfileFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment, profileFragment)
+                        .commit()
+                }
+            }
+        }
+    }
+
+    private fun setupButton() {
         binding.btn.setOnClickListener {
             startActivity(Intent(this@MainActivity, WelcomeActivity::class.java))
         }
